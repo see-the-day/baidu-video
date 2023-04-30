5e934faceeb8625d59b6976389d77f25517d4047
mengfanlin <mengfanlin@smzdm.com>
2023-05-01 02:27:49 +0800

    图片优化

--- src/view/index/imgBox.vue
+++ src/view/index/imgBox.vue
@@ -1,12 +1,12 @@
 <template>
   <n-grid :x-gap="8" :y-gap="8" :cols="2">
     <n-grid-item
-      v-for="({ img }, index) of list"
+      v-for="({ img, text }, index) of list"
       :key="index"
       class="flex h-70 cursor-pointer items-center justify-center border text-textWhite"
       @click="addImg(img)"
     >
-      <span>默认</span>
+      <span>{{ text }}</span>
     </n-grid-item>
   </n-grid>
 </template>
@@ -14,8 +14,14 @@
 import { NGrid, NGridItem } from 'naive-ui'
 import { useState } from '@/store/videoState'
 import vueImg from '@/assets/map-img/logo.svg'
+import xiaomai from '@/assets/map-img/xiaomai.gif'
+import xiaoxiong from '@/assets/map-img/xiaoxiong.gif'
 
-const list = [{ img: vueImg }]
+const list = [
+  { text: '默认', img: vueImg },
+  { text: '小埋', img: xiaomai },
+  { text: '小熊', img: xiaoxiong }
+]
 
 const state = useState()
 const addImg = (img: string) => {
